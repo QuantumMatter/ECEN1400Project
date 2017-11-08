c832c976e33040ddc5c052befdf7586b55474025
David Kopala <kopala.david@gmail.com>
2017-11-08 20:29:53 +0000

    Added build script for RPi, fixed RPiMaster, updated ip email to read gmail password from a text file

--- RaspberryPi/RPiMaster/RPiMaster/main.cpp
+++ RaspberryPi/RPiMaster/RPiMaster/main.cpp
@@ -11,9 +11,10 @@
 #include <wiringPi.h>
 #include "Client.h"
 
-#define SERV_ADDR   "127.0.0.1"//"52.38.18.162"
+#define SERV_ADDR   "52.38.18.162"
 #define SERV_PORT   15003
 #define LED         0
+//Pin 0 => GPIO Pin BCM 17
 
 using namespace std;
 
@@ -24,17 +25,19 @@ void clientReadCallback(char *message) {
         if (message[0] == 'o') {
             //Turn LED On
             digitalWrite(LED, 1);
+	    cout<<"turning LED 0 on"<<endl;
         } else if (message[0] == 'f') {
             //Turn LED Off
             digitalWrite(LED, 0);
+	    cout<<"turning LED 0 off"<<endl;
         }
     }
 }
 
 void streamReqCallback(char *message) {
     if (strcmp(message, "connected") == 0) {
-        Client *client = new Client(SERV_ADDR, SERV_PORT);
-        client->readCallback = clientReadCallback;
+       // Client *client = new Client(SERV_ADDR, SERV_PORT);
+       // client->readCallback = clientReadCallback;
     } else {
         cout<<"could not connect"<<endl;
     }
@@ -53,10 +56,16 @@ int main(int argc, const char * argv[]) {
     //Configure WiringPi GPIO utility
     wiringPiSetup();
     pinMode(LED, OUTPUT);
+
+   //Wait half second to allow server to open port
+   delay(500);
+   Client *client = new Client(SERV_ADDR, SERV_PORT);
+   client->readCallback = clientReadCallback;
     
+    cout<<"starting loop"<<endl;
     while (true) {
-        
-    }
+
+   }
     
     
     return 0;
