bbac6095636b7978e2d83e834e2a9d569f68276d
QuantumMatter <kopala.david@gmail.com>
2017-12-03 20:28:36 -0700

    updated build script

--- RaspberryPi/RPiMaster/RPiMaster/build.sh
+++ RaspberryPi/RPiMaster/RPiMaster/build.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 -lwiringPi -pthread main.cpp Client.h Client.cpp List.h
+g++ -std=c++11 -lwiringPi -pthread main.cpp Client.h Client.cpp List.h L293D.hpp L293D.cpp ArduinoSlave.hpp ArduinoSlave.cpp
