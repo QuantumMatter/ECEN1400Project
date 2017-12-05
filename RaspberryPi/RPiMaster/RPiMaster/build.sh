c455c635152ad990e18c78286a08d522a85f130f
QuantumMatter <kopala.david@gmail.com>
2017-12-04 23:21:10 -0700

    Updated build script; started using python scripts for i2c devices

--- RaspberryPi/RPiMaster/RPiMaster/build.sh
+++ RaspberryPi/RPiMaster/RPiMaster/build.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 -lwiringPi -pthread main.cpp Client.h Client.cpp List.h L293D.hpp L293D.cpp ArduinoSlave.hpp ArduinoSlave.cpp
+g++ -std=c++11 -lwiringPi -pthread main.cpp Client.h Client.cpp List.h L293D.hpp L293D.cpp ArduinoSlave.hpp ArduinoSlave.cpp TrekManager.hpp TrekManager.cpp MPL3115A2.hpp MPL3115A2.cpp -lcurl
